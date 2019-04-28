1ee5bc27944b70853f1f7ba6b9af4a94a49faa8a
Christian Soler <solero93@gmail.com>
2019-04-28 15:39:42 +0200

    Using Counter in the popularity bias test

--- test/bias/PopularityBiasTest.py
+++ test/bias/PopularityBiasTest.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import Counter
 from typing import Dict
 
 from pandas import Series
@@ -9,14 +10,15 @@ from src.bias.PopularityBias import PopularityBias
 class PopularityBiasTest(unittest.TestCase):
     def test_analyze(self):
         # GIVEN
-        input_data_frame: Series = Series(data=[2, 3, 2])
+        input_data = [2, 3, 2, 2, 1]
+        series: Series = Series(data=input_data)
         popularity_bias: PopularityBias = PopularityBias()
 
         # WHEN
-        actual: Dict = popularity_bias.analyze(input_data_frame)
+        actual: Dict = popularity_bias.analyze(series)
 
         # THEN
-        expected: Dict = {2: 2, 3: 1}
+        expected: Dict = Counter(input_data)
         self.assertDictEqual(actual, expected)
 
 
