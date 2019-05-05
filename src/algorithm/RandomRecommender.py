afc5a52991e3073aa3a584943d147eb6d7a5b724
Christian Soler <solero93@gmail.com>
2019-05-05 14:34:59 +0200

    Fixed returning count of elements of ndarray

--- src/algorithm/RandomRecommender.py
+++ src/algorithm/RandomRecommender.py
@@ -11,5 +11,5 @@ class RandomRecommender(RecommenderStrategy):
         all_movies: np.ndarray = np.unique(input_data['movieId'].get_values())
         all_users: np.ndarray = np.unique(input_data.index.get_values())
         all_recommendations: np.ndarray = np.random.choice(all_movies, size=(all_users.size, 10))
-        frequency_of_recommendations: np.ndarray = np.bincount(all_recommendations.flatten())
+        _, frequency_of_recommendations = np.unique(all_recommendations.flatten(), return_counts=True)
         return (-np.sort(-frequency_of_recommendations)).tolist()
--- src/bias/BiasStrategy.py
+++ src/bias/BiasStrategy.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import List
 
 from pandas import Series
 
 
 class AnalyzeBiasStrategy(ABC):
     @abstractmethod
-    def analyze(self, input_data: Series) -> Dict:
+    def analyze(self, input_data: Series) -> List[int]:
         pass
