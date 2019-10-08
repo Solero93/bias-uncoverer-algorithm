6c1187fb0a769dafc492a9ea723642e9c245b70e
Christian Soler <solero93@gmail.com>
2019-10-08 20:44:49 +0200

    Added rabbit host for docker

--- src/infrastructure/rabbitmq/connection.py
+++ src/infrastructure/rabbitmq/connection.py
@@ -3,5 +3,5 @@ import pika
 
 def get_connection() -> pika.BlockingConnection:
     credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
-    parameters = pika.ConnectionParameters(credentials=credentials)
+    parameters = pika.ConnectionParameters(host='rabbit', credentials=credentials)
     return pika.BlockingConnection(parameters=parameters)  # TODO Change to poll every so many seconds
