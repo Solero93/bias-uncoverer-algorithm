3749835fb906a497ffc988e172e0396e87795b7c
Christian Soler <solero93@gmail.com>
2019-10-08 20:37:52 +0200

    Added exponential backoff

--- src/infrastructure/AnalysisQueryProcessor.py
+++ src/infrastructure/AnalysisQueryProcessor.py
@@ -1,3 +1,7 @@
+from time import sleep
+
+import pika
+
 from src.application.AnalyzeAlgorithmBias import AnalyzeAlgorithmBias
 from src.application.AnalyzeDataBias import AnalyzeDataBias
 from src.domain.AnalysisQuery import AnalysisQuery
@@ -5,6 +9,7 @@ from src.domain.AnalysisResult import AnalysisResult
 from src.domain.repositories.AnalysisQueryRepository import AnalysisQueryRepository
 from src.domain.repositories.AnalysisResultRepository import AnalysisResultRepository
 from src.domain.value_objects.Graph import Graph
+from src.infrastructure.rabbitmq.connection import get_connection
 from src.infrastructure.repositories.analysis_query.RabbitMQAnalysisQuery import RabbitMQAnalysisQuery
 from src.infrastructure.repositories.analysis_result.RabbitMQAnalysisResult import RabbitMQAnalysisResult
 
@@ -15,7 +20,26 @@ class AnalysisQueryProcessor:
         self.analysis_query_repository = analysis_query_repository
         self.analysis_result_repository = analysis_result_repository
 
+    def _wait_for_connection_to_be_open_or_exit(self):
+        wait_time = 1
+        success = False
+
+        while wait_time < 100 and not success:
+            try:
+                connection: pika.adapters.BlockingConnection = get_connection()
+                connection.close()
+                success = True
+            except pika.connection.exceptions.AMQPConnectionError:
+                sleep(wait_time)
+                wait_time *= 2
+
+        if not success:
+            print("Couldn't connect to rabbit")
+            exit(1)
+
     def run(self):
+        self._wait_for_connection_to_be_open_or_exit()
+
         print("Waiting for query...")
         analysis_query: AnalysisQuery = self.analysis_query_repository.get_query()
         print("Query obtained!", analysis_query)
